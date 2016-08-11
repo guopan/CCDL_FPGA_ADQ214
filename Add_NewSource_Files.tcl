ec4a63e7a1b029e7d37eb9bedb4722617b7e2fb1
guopan <guopanbit@gmail.com>
2016-08-11 15:37:55 +0800

    增加了使用说明与“用于添加文件”的tcl文件

---
+++ Add_NewSource_Files.tcl
@@ -0,0 +1,46 @@
+set myScript "Add_NewSource_Files.tcl"
+set myProject "ADQ214_devkit"
+set myFPGA "alg_sx50t"
+set DesignName "adq_alg_fpga"
+
+# 
+# add_source_files
+# 
+# This procedure add the source files that were known to the project at the
+# time this script was generated.
+# 
+proc add_newsource {} {
+
+   global myScript
+   global myProject
+   global myFPGA
+   global DesignName
+
+   putstatus $myScript "Adding sources to project..."
+   project open ../implementation/xilinx/${myProject}.xise
+   
+   xfile add "../../source/FIFO_In.v"
+   xfile add "../../source/FIFO_TC.v"
+   xfile add "../../source/Power_Spec_Cal.v"
+   xfile add "../../source/RangeBin_Counter.v"
+   xfile add "../../source/SPEC_Acc.v"
+   xfile add "../../source/Trigger_Decoder.v"
+   xfile add "../../source/userlogical_processing_tb.v"
+   xfile add "../../source/ipcore_dir/DPRAM_Buffer.xco"
+   xfile add "../../source/ipcore_dir/fifo_Buffer_in.xco"
+   xfile add "../../source/ipcore_dir/Fifo_Buffer_Tc.xco"
+   xfile add "../../source/ipcore_dir/Multiplier_16.xco"
+   xfile add "../../source/ipcore_dir/xfft_v7_1.xco"
+   
+
+   # Set the Top Module as well...
+   project set top "adq_alg_fpga"
+
+   putstatus $myScript "Project sources reloaded."
+
+} ; # end add_source_files
+
+proc putstatus {ScriptName Status} {
+    set CurrentTime [clock format [clock seconds] -format {%Y-%m-%d %H:%M:%S}]
+    puts "$ScriptName: $CurrentTime $Status"
+}
--- README.md
+++ README.md
@@ -4,22 +4,35 @@ Implement FFT algorithm in ADQ214 FPGA #1.
 And also the power spectrum accumulation, Peak detection algorithm.
 
 ##目录结构如下：
-
+```
 ├─implementation  
-│　└─xilinx  
-│　　　├─constraints  
-│　　　├─ipcore_dir  
-│　　　├─iseconfig  
-│　　　├─isim  
-│　　　├─logfiles  
-│　　　├─netgen  
-│　　　├─scripts  
-│　　　├─xlnx_auto_0_xdb  
-│　　　├─xst  
-│　　　├─_ngo  
-│　　　└─_xmsgs  
+│  └─xilinx  
+│      ├─constraints  
+│      ├─ipcore_dir  
+│      ├─iseconfig  
+│      ├─isim  
+│      ├─logfiles  
+│      ├─netgen  
+│      ├─scripts  
+│      ├─xlnx_auto_0_xdb  
+│      ├─xst  
+│      ├─_ngo  
+│      └─_xmsgs  
 ├─ngc  
 └─source  
-　　└─ipcore_dir   
+    ├─Matlab_verify  
+    └─ipcore_dir
+```
 
-Git仓库位于source文件夹
+Git仓库位于source文件夹、
+
+# 使用方法
+
+1. 将ADQ214开发包复制到适当位置，按照User Guide文件“11-0724_UsersGuide_ADQ_V5_DevKit.pdf”中的说明，利用ADQ214_devkit.tcl创建ISE工程。
+2. 从Github网站clone项目文件到source文件夹，执行其中的Add_NewSource_Files.tcl，添加新的源文件到工程之中。
+3. Regenerate All cores，其间每个IP core会弹出缺失xxx_synth.vhd的对话框，选择Yes继续。
+
+# 开发软件版本：
+
+ISE 14.7 （会影响其中的IP core 生成的版本）
+在低版本的ISE中，Regenerate Al IPl cores可能会不成功。
